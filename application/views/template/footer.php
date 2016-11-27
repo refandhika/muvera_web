5f9f3c2bd51e2d66c7e796193ce96dedfa52cca7
refandhika <refandika@gmail.com>
2016-11-28 02:07:21 +0700

    footer fix

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,9 +1,11 @@
-	
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery-3.1.0.js"></script>
-	<script src="assets/js/d3.v3.min.js"></script>
-	<script src="assets/js/rickshaw.min.js"></script>
-	<script src="assets/js/d3.layout.cloud.js"></script>
-	<script src="assets/js/assets.js"></script>
+		<div class="content-footer">
+			<div class="footer-bg"></div>
+			<div class="footer-tx rotate">Lorem Ipsum Dolor</div>
+		</div>
+	</div>
+	<script src="<?php echo base_url('assets/js/jquery-1.12.3.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery-ui.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/js/assets.js'); ?>"></script>
 </body>
 </html>
